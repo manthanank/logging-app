aa379475f2fd61d9c270d44498f187af0d7e4ea6
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-07-04 13:14:05 +0530

    chore: Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    logApiUrl: window.location.hostname === 'localhost' ? 'http://localhost:3000/api/logs' : 'https://blog-app-nh06.onrender.com/api/logs'
+    logApiUrl: window.location.hostname === 'localhost' ? 'http://localhost:3000/api/logs' : 'https://backend-app-8ev9.onrender.com/api/logs'
 };
